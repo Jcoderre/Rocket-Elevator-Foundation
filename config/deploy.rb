ae28b217e88de02c0ab45b74fc4fc9d6383e4dd8
Jcoderre <joeysteam@outlook.com>
2020-11-19 10:30:41 -0500

    change to ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.14.1"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "JoeyCoderre"
-set :repo_url, "https://github.com/Jcoderre/Rocket-Elevator-Foundation"
+set :repo_url, "git@github.com:Jcoderre/Rocket-Elevator-Foundation.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
