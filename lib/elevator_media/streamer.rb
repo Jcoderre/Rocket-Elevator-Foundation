e350f8a603d52c7cb5185f250e6959b18beda92c
Jcoderre <joeysteam@outlook.com>
2020-11-25 10:03:20 -0500

    TTD ElevatorMedia

--- lib/elevator_media/streamer.rb
+++ lib/elevator_media/streamer.rb
@@ -1,36 +1,33 @@
+# require 'rails_helper'
 require 'uri'
 require 'net/http'
 require 'openssl'
-class Streamer 
 
-    def bing_news_streamer        
-        url = URI("https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw")
-        
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        
-        request = Net::HTTP::Get.new(url)
-        request["x-bingapis-sdk"] = 'true'
-        request["x-rapidapi-key"] = ENV['bingapis_key']
-        request["x-rapidapi-host"] = 'bing-news-search1.p.rapidapi.com'
-        
-        response = http.request(request)
-        puts response.read_body
-    end
+module ElevatorMedia
+    class Streamer 
+        def self.bing_news_streamer        
+            url = URI("https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw")
+            
+            http = Net::HTTP.new(url.host, url.port)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    def getContent
-
-    end
-
-    ## TESTING PART
-    def self.add(input)
-        if input.empty?
-            0
-        else 
-            numbers = input.split(",").map { |num| num.to_i }
-            numbers.inject(0) { |sum, number| sum + number }
+            request = Net::HTTP::Get.new(url)
+            request["x-bingapis-sdk"] = 'true'
+            request["x-rapidapi-key"] = '02fcb00b00mshca77b871d58f815p1b82d4jsn385d5be92e0a'
+            request["x-rapidapi-host"] = 'bing-news-search1.p.rapidapi.com'
+            
+            response = http.request(request)
+            JSON.parse(response.read_body)
+        end
+            
+        def self.getContent()
+            content = "<div><p>"
+            news = bing_news_streamer['value'][0]
+            content = content + '<p>' + news['name'] + "</p>"
+            content = content + news['description'] + "</div>"
+            content = content + "<div><p>" + news['datePublished']+ '</p>'"</div>"
+            content
         end
     end
-
 end
--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -1,25 +1,48 @@
 require 'rails_helper'
 require 'elevator_media/streamer.rb'
 
-describe Streamer do
+describe ElevatorMedia::Streamer do 
 
-    it "should work" do
-        expect(true).to be_truthy
-    end
-
-    describe ".add" do
-        context "two numbers" do
-            context "given '2,4'" do
-                it "returns 6" do
-                    expect(Streamer.add("2,4")).to eql(6)
-                end
-             end
-    
-            context "given '10,107'" do
-                it "returns 117" do
-                expect(Streamer.add("10,107")).to eql(117)
-                end
-            end
+    describe "Streamer is initialized" do
+        it "Should initialize" do
+            streamer = ElevatorMedia::Streamer.new
+            expect(streamer).to be_a(ElevatorMedia::Streamer)
         end
     end
+
+    describe "getContent" do
+        it "should return a string" do
+            expect(ElevatorMedia::Streamer.getContent()).to be_a(String)       
+        end
+        it "should begin with a div tag" do
+            content = ElevatorMedia::Streamer.getContent()    
+            expect(content).to end_with('</div>')  
+        end
+        it "should include p tag" do
+            content = ElevatorMedia::Streamer.getContent()
+            expect(content).to include('<p>')
+        end
+        it "should include integer" do
+            content = ElevatorMedia::Streamer.getContent()
+            expect(content).to include("2020")
+        end
+        it "Deliberate Failure (text ending with a </p> instead of </div>" do
+            content = ElevatorMedia::Streamer.getContent()
+            expect(content).to end_with("</p>")
+        end
+    end
+
+    describe 'bing_news_streamer' do
+        it "should have a message" do 
+            response = ElevatorMedia::Streamer.bing_news_streamer()
+            expect(response).to include("message")     
+        end
+        it 'should have a string' do
+            http = ElevatorMedia::Streamer.bing_news_streamer()
+            expect(http).to include(String)
+        end
+    end
+
 end 
+
+describe 
