c608a0682c7e6911556d49da109c647ba7bc3954
Jcoderre <joeysteam@outlook.com>
2020-11-27 17:08:07 -0500

    add mock and start elevator_media test

--- spec/elevator_media_spec.js
+++ spec/elevator_media_spec.js
@@ -1,7 +1,21 @@
-require 'rails_helper'
-require 'elevator_media/streamer.rb'
+
+import streamer from "./elevator_media";
 
 
+const http = require('http');
+
+export default function request(url) {
+  return new Promise(resolve => {
+    // This is an example of an http request, for example to fetch
+    // user data from an API.
+    // This module is being mocked in __mocks__/request.js
+    http.get({path: url}, response => {
+      let data = '';
+      response.on('data', _data => (data += _data));
+      response.on('end', () => resolve(data));
+    });
+  });
+}
 
 describe ElevatorMedia::Streamer do 
 
