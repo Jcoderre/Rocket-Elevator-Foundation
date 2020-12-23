90d50ddbf071015d1e8fe989be8920532cc3b6e5
Jcoderre <joeysteam@outlook.com>
2020-12-23 15:48:58 -0500

    add newrelic

--- __tests__/streamer.js
+++ __tests__/streamer.js
@@ -1,16 +1,16 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw",
+fetch("https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw", {
 	"method": "GET",
 	"headers": {
 		"x-bingapis-sdk": "true",
 		"x-rapidapi-key": "02fcb00b00mshca77b871d58f815p1b82d4jsn385d5be92e0a",
 		"x-rapidapi-host": "bing-news-search1.p.rapidapi.com"
 	}
-};
-
-$.ajax(settings).done(function (response) {
+})
+.then(response => {
 	console.log(response);
+})
+.catch(err => {
+	console.error(err);
 });
 
+module.exports = response;
